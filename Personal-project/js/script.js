5d4b61f6284fa8373ea4e745f2cd69fe4555724e
Dmitriy <ggbox@mail.ru>
2020-02-27 18:15:40 +0400

    кнопки на ссылки поменял
    добавил модалку
    поменял кнопки в тойс
    вроде больше не прыгают текст инпуты
    ширину пока не трогал

---
+++ Personal-project/js/script.js
@@ -0,0 +1,15 @@
+let modalButton = document.querySelector('.modal-button_js');
+let modalWindow = document.querySelector('.modal-window');
+let closeModalButton = document.querySelector('.close-form-button');
+
+function showModal(){
+    modalWindow.classList.toggle('hidden')
+};
+
+modalButton.addEventListener ('click', function (evt) {
+    evt.preventDefault(); showModal();
+ });
+
+closeModalButton.addEventListener ('click', function (evt) {
+    evt.preventDefault(); showModal();
+ });
